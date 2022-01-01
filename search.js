f06b4ae721c994db842404c26a29e0ea7334778b
Alex <47290210+WanderWalsh@users.noreply.github.com>
2021-12-31 23:38:29 -0700

    Create search.js

---
+++ search.js
@@ -0,0 +1,40 @@
+/** @param {NS} ns **/
+export async function main(ns) {
+    //ns.scan("home");
+	var serverName= "";
+	if(ns.args.length == 0){
+		serverName = "home";
+	}
+	else{
+		serverName = ns.args[0];
+	}
+	var hostNames = ns.scan(serverName);
+
+	for(var i = 0; i<hostNames.length; i++ ){
+		while(true){	
+			var fram = await ns.getServerMaxRam("home") - ns.getServerUsedRam("home");
+			if (fram > 4 ){
+			await ns.exec("crack.js", "home",1,hostNames[i]);
+			break;
+			}
+			else{
+				await ns.sleep(1000)
+			}
+		}
+	}	 
+	for(var i = 0; i<hostNames.length; i++ ){
+		while(true){	
+			var fram = await ns.getServerMaxRam("home") - ns.getServerUsedRam("home");
+			if (fram > 3 ){
+				if(hostNames[i] == "home" || (ns.args.length > 1 && ns.args[1]!= hostNames[i])){
+					break;
+				}
+				await ns.exec("search.js", "home",1,hostNames[i], serverName);
+				break;
+			}
+			else{
+				await ns.sleep(1000)
+			}
+		}
+	}
+}
