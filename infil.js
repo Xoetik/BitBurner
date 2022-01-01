8594d3e1ebf560ee9cf6f12b0955b1ad19100705
Alex <47290210+WanderWalsh@users.noreply.github.com>
2021-12-31 23:37:24 -0700

    Create infil.js

---
+++ infil.js
@@ -0,0 +1,21 @@
+/** @param {NS} ns **/
+export async function main(ns) {
+    var target = "joesguns";
+    var moneyThresh = ns.getServerMaxMoney(target) * 0.75;
+    var secThresh = ns.getServerMinSecurityLevel(target) + 5;
+
+    while(true){
+        
+        if(ns.getServerSecurityLevel(target) > secThresh){
+            await ns.weaken(target);
+        
+
+        }
+        else if( ns.getServerMoneyAvailable(target) < moneyThresh){
+            await ns.grow(target);
+        }
+        else{
+            await ns.hack(target);
+        }
+    }
+}
