bdb4121dccd482d0abc67152dc5ed5a1daf55c27
DaTimeKeeper <zachwalsh2219@live.com>
2022-01-05 02:35:37 -0700

    Alert Comment

--- Buy_Server/perServ.js
+++ Buy_Server/perServ.js
@@ -4,13 +4,13 @@ export async function main(ns) {
 	if(ns.serverExists("pserv-0")){
 		curentRam = ns.getServerMaxRam("pserv-0");
 	}
-	ns.alert("Current Ram: "+curentRam);
+	// ns.alert("Current Ram: "+curentRam);
 	
 	
 	var x = ns.getServerMoneyAvailable("home")*0.75/ns.getPurchasedServerCost(1)/25;
     var y =Math.floor(Math.log(x)/Math.log(2));
     var ram = Math.pow(2,y);
-	ns.alert("Upgrade Ram: "+ram);
+	// ns.alert("Upgrade Ram: "+ram);
 
 	if(curentRam < ram){
 		await ns.exec("delServ.js", "home");
