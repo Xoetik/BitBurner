ae13c59ebafc566f8c79fb892d709c4ac14b3942
DaTimeKeeper <zachwalsh2219@live.com>
2022-01-10 02:53:33 -0700

    Organized and optimized

--- distAttacks/dAttack.js
+++ distAttacks/dAttack.js
@@ -1,65 +1,117 @@
 /** @param {NS} ns **/
 export async function main(ns) {
-    var serverName = "";
-	var target = "home";
-	if (ns.args.length == 0) {
-		serverName = "home";
-	}
-	else {
-		serverName = ns.args[0];
-	}
-	var hosts = ns.scan(serverName);
+    let hosts=await getHosts(ns);
+    let targets=await filter(hosts,ns.getPurchasedServers()); 
+    targets=await sortByMoneyPerSecond(ns,targets);
+    var scriptRam=ns.getScriptRam("/distAttacks/wag.js","home");
+    hosts=await removeBadHosts(ns,hosts,scriptRam);
+    await wagAlgo(ns,targets,hosts,scriptRam);
+    await ns.tprint("dAttack.js complete!");
+}
 
-	for (var i = 0; i < hosts.length; i++) {
-		await ns.sleep(100);
-		var adder = ns.scan(hosts[i]);
-		for (var j = 0; j < adder.length; j++) {
+
+async function getHosts(ns){
+	let hosts = ns.scan("home");
+    for (let i = 0; i < hosts.length; i++) {
+		// await ns.sleep(100);
+		let adder = ns.scan(hosts[i]);
+		for (let j = 0; j < adder.length; j++) {
 			if (!hosts.includes(adder[j])) {
 				hosts.push(adder[j]);
 			}
 		}
 	}
-   
-    var targetList= [];
-    for(var i =0; i< hosts.length; i++){
-        var name = hosts[i];
-        var hackTime = ((2.5 * ns.getServerRequiredHackingLevel(hosts[i]) * (50 +(ns.getServerMinSecurityLevel(hosts[i])/2))+ 500) * 5 
+    hosts=filter(hosts,["home"]);
+    return hosts;
+}
+
+async function filter(orig,removal){
+    var fin=[];
+    for (let i = 0; i < orig.length; i++) {
+        if (!removal.includes(orig[i])) {
+            fin.push(orig[i]);
+        }      
+    }
+    return fin;
+}
+
+async function sortByMoneyPerSecond(ns,targets){
+    var targetList=[];
+
+    for(var i =0; i< targets.length; i++){
+        var sName = targets[i];
+        var hackTime = ((2.5 * ns.getServerRequiredHackingLevel(sName) * (50 +(ns.getServerMinSecurityLevel(sName)/2))+ 500) * 5 
                             / (ns.getHackingLevel()+50))/ ns.getPlayer().hacking_speed_mult;
-        var maxMoney = ns.getServerMaxMoney(hosts[i]); 
-
-        if(ns.getServerMoneyAvailable(hosts[i])<=0 || !ns.hasRootAccess(hosts[i]) || hosts[i] == "home" || ns.getServerRequiredHackingLevel(hosts[i])> ns.getHackingLevel() || hackTime > 60){
-            continue;
+        var maxMoney = ns.getServerMaxMoney(sName);
+        if(hackTime>300){
+            maxMoney=maxMoney/1000;
+        }else if(hackTime>60){
+            maxMoney=maxMoney/10;
+        } 
+        if(ns.getServerMoneyAvailable(sName)>0 && ns.hasRootAccess(sName) && ns.getServerRequiredHackingLevel(sName) <= ns.getHackingLevel()){
+            targetList.push({name:sName, money:(maxMoney*0.004/hackTime)});
         }
-        else{
-            targetList.push([name, maxMoney]);
-        }
-        
-    }        
-
-    for (i = 0; i < targetList.length-1; i++){
-        for (j = 0; j < targetList.length-i-1; j++){
-            if (targetList[j][1] < targetList[j+1][1]){
-
-                var temp = targetList[j];
+    }
+    
+    for (let i = 0; i < targetList.length-1; i++){
+        for (let j = 0; j < targetList.length-i-1; j++){
+            if (targetList[j].money < targetList[j+1].money){
+                let temp = targetList[j];
                 targetList[j] = targetList[j+1];
                 targetList[j+1] = temp;
             
             }
         }
+        
     }
-
-    var targets =[];
+    var fin =[];
     for(var i = 0; i<targetList.length; i++){
-       targets.push(targetList[i][0]);
-
+       fin.push(targetList[i].name);
     }
+    return fin;
+}
 
-    var pServNames = ns.getPurchasedServers();
-    var scriptRam=ns.getScriptRam("/distAttacks/wag.js","home");
-    var maxThreadsPerServ = ns.getServerMaxRam(pServNames[0])/scriptRam;
+async function removeBadHosts(ns,hosts,ram){
+    var fin=[];
+    for (let i = 0; i < hosts.length; i++) {
+        if(!ns.hasRootAccess(hosts[i])){
+            await open(ns,hosts[i]);
+        }
+        if (ns.getServerMaxRam(hosts[i])>ram&&ns.hasRootAccess(hosts[i])) {
+            fin.push(hosts[i]);
+        }      
+    }
+    return fin;
+}
+async function open(ns,target){
+    var ports = 0;
+	if(ns.fileExists("BruteSSH.exe", "home")){
+		await ns.brutessh(target);
+		ports++;
+	}
+	if(ns.fileExists("FTPCrack.exe", "home")){
+		await ns.ftpcrack(target);
+		ports++;
+	}
+	if(ns.fileExists("relaySMTP.exe", "home")){
+		await ns.relaysmtp(target);
+		ports++;
+	}
+	if(ns.fileExists("HTTPWorm.exe", "home")){
+		await ns.httpworm(target);
+		ports++;
+	}
+	if(ns.fileExists("SQLInject.exe", "home")){
+		await ns.sqlinject(target);
+		ports++;
+	}
+	if(ns.getServerNumPortsRequired(target)<= ports){
+		await ns.nuke(target);
+	}
+}
+async function wagAlgo(ns,targets,hosts,ram){
     var servUsed=0;
     var servsAttacking=0;
-    var targets=["n00dles","joesguns","foodnstuff","sigma-cosmetics","hong-fang-tea","harakiri-sushi","iron-gym","nectar-net","phantasy","neo-net","crush-fitness","johnson-ortho"];
     var targetServ=[];
     var hostServ=[];
     for(let i=0;i<targets.length;i++){
@@ -67,42 +119,43 @@ export async function main(ns) {
         targetServ.push({name:targets[i],threads:tMax});
     }
     for(let i=0;i<hosts.length;i++){
-        let tMax =ns.getServerMaxRam(hosts[servsAttacking])/scriptRam;
+        let tMax =ns.getServerMaxRam(hosts[i])/ram;
+
         hostServ.push({name:hosts[i],threads:tMax});
         await ns.scp("/distAttacks/wag.js", hosts[i]);
         await ns.killall(hosts[i]);
+        await ns.sleep(10);
     }
-
+    await ns.sleep(1000);
     while(hostServ.length>servUsed&&targetServ.length>servsAttacking){
         if(targetServ[servsAttacking].threads>hostServ[servUsed].threads){
-            await(ns.tprint(servsAttacking));
             targetServ[servsAttacking]={name:targetServ[servsAttacking].name,threads:targetServ[servsAttacking].threads-hostServ[servUsed].threads};
             let t =hostServ[servUsed].threads;
-            await ns.scp("/distAttacks/wag.js", hosts[servUsed]);
+            await ns.scp("/distAttacks/wag.js", hostServ[servUsed].name);
             await ns.exec("/distAttacks/wag.js", hostServ[servUsed].name, t, targetServ[servsAttacking].name); 
             servUsed++;
         }else{
-            await(ns.tprint(servsAttacking));
             hostServ[servUsed]={name:hostServ[servUsed].name,threads:hostServ[servUsed].threads-targetServ[servsAttacking].threads};
             let t=targetServ[servsAttacking].threads;
-            await ns.scp("/distAttacks/wag.js", hosts[servUsed]);
+            await ns.scp("/distAttacks/wag.js", hostServ[servUsed].name);
             await ns.exec("/distAttacks/wag.js", hostServ[servUsed].name, t, targetServ[servsAttacking].name); 
             servsAttacking++;
         }
     }
-
-    await(ns.tprint(servsAttacking));
+    ns.scriptKill("/distAttacks/collectMoney.js","home");
     for(var k=0;k<servsAttacking+1;k++){
-        await ns.sleep(100);
+        var [totalRam, ramUsed] = ns.getServerRam("home");
+        var availRam=totalRam-ramUsed;
         let balanceFactor = 240;
         let difficultyMult = (100 - (ns.getServerMinSecurityLevel(targetServ[k].name)+5)) / 100;
         let skillMult = (ns.getPlayer().hacking - (ns.getServerRequiredHackingLevel(targetServ[k].name)- 1)) / ns.getPlayer().hacking;
         let perc = (difficultyMult * skillMult * ns.getPlayer().hacking_money_mult) / balanceFactor;
         var thre=.75/perc;
-        await ns.kill("/distAttacks/collectMoney.js","home",targetServ[k].name);
-        await ns.exec("/distAttacks/collectMoney.js", "home", thre, targetServ[k].name);
+        if(availRam>=thre){
+            await ns.exec("/distAttacks/collectMoney.js", "home", thre, targetServ[k].name);
+        }else{
+            await ns.exec("/distAttacks/collectMoney.js", "home", availRam, targetServ[k].name);
+            break;
+        }
     }
-
-    await ns.tprint("dAttack.js complete!");
-	 
 }
