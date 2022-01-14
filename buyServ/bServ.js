622a4b1ec2d4e69960c5ea01520fb1479f921add
DaTimeKeeper <zachwalsh2219@live.com>
2022-01-14 01:24:53 -0700

    adds buying one large server at a time

---
+++ buyServ/bServ.js
@@ -0,0 +1,49 @@
+/** @param {import("../.").NS} ns */
+
+export async function main(ns) {
+	var percent = 0.85;
+	if(ns.args.length != 0){
+		percent = ns.args[0];
+	}
+	var pServers=ns.getPurchasedServers();
+    var x = ns.getServerMoneyAvailable("home")*percent/ns.getPurchasedServerCost(1);
+    var y =Math.floor(Math.log(x)/Math.log(2));
+    var ram = Math.pow(2,y);
+    for (let i = 0; i < pServers.length-1; i++){
+        for (let j = 0; j < pServers.length-i-1; j++){
+            if (ns.getServerMaxRam(pServers[j]) > ns.getServerMaxRam(pServers[j+1])){
+                let temp = pServers[j];
+                pServers[j] = pServers[j+1];
+                pServers[j+1] = temp;
+            
+            }
+        }  
+    }
+    var repeatFlag=false;
+    if(ram>1048576){
+        ram=1048576;
+        repeatFlag=true;
+    }
+	if(pServers.length<25){
+		ns.purchaseServer("pserv", ram);
+        await ns.tprint("New server bought");
+	}else{
+        for (let i = 0; i < pServers.length; i++){
+            if (ns.getServerMaxRam(pServers[i]) < ram){
+                ns.killall(pServers[i]);
+                ns.deleteServer(pServers[i]);
+                ns.purchaseServer("pserv", ram);
+                await ns.tprint("Server updated");
+                break;
+            }
+            if(i==24){
+                repeatFlag=false;
+            }
+        }
+    }
+    if(repeatFlag==true){
+        await ns.exec("/buyServ/bServ.js", "home",percent);
+    }else{
+        await ns.exec("/distAttacks/dAttack.js", "home");
+    }  
+}
